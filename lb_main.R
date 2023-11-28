c191168e628051789c81c98fb353ad5d66e0770e
Luke Beebe <97060937+lukebeebe@users.noreply.github.com>
2023-11-28 18:52:34 -0500

    Add files via upload

--- lb_main.R
+++ lb_main.R
@@ -1,15 +1,17 @@
 # Weather Prediction by Luke, Kavi
 library(lars)
 
+# NORMALIZE DATA PER SEASON
+
 # main
-weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F, split=0.7){
+weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F){
   df <- data[, append(x, y)]
   rownames(df) <- seq(1, nrow(df), 1)
   if(norm==T){df <- scale(df[, x])}
   df <- delay_map(df, lag)
 
-  # split data into past/future
-  split <- floor(split*nrow(df))
+  # split data into past/future 9 seasons
+  split <- nrow(df)-9
   df_train <- df[1:split,]
   df_test <- df[(split+1):nrow(df),]
   
@@ -20,7 +22,7 @@ weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F, split=
   
   # random samples to pull from columns
   sample_cols <- delay_sample(ntrial=1200, (ncol(df_train)-1), dim)
-  par(mfrow=c(length(year),length(seas)))
+  par(mfrow=c(length(seas),length(year)))
   info <- NULL
   resids <- NULL
   
@@ -38,14 +40,18 @@ weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F, split=
         Y <- nn_train[, y]
         lars_output <- lars(X, Y)
         model_col <- match(TRUE, lars_output$Cp==min(lars_output$Cp))
+        if(is.na(model_col)){
+          #TROUBLESHOOT NA VALUES
+          next
+        }
         ypred <- predict(lars_output, X[1:5,])$fit[, model_col]
-        #TROUBLESHOOT NA VALUES
         resid <- Y[1:5] - ypred
         info <- append(info, list(ypred, resid, sample_cols[[k]]))
         resids <- append(resids, resid)
       }
+      #TROUBLESHOOT NA VALUES
       resids <- na.omit(resids)
-      print(paste("NA values:", 6000-length(resids)))
+      print(paste("NA values:", (6000-length(resids))/5))
       den <- density(resids)
       plot(den, frame=T, col='blue',main=paste(j,i))
       resids<-NULL
@@ -91,5 +97,5 @@ nn_finder <- function(x, y, nn){
   return(nn_cols)
 }
 
-weather_prediction(hawaii_data, x=2:7, y=1, seas=c(1,3,4), year=c(1,2), lag=6, nn=30, norm=F, split=0.7)
+weather_prediction(hawaii_data, x=2:7, y=1, seas=c(1,3), year=c(1,2), lag=6, nn=30, norm=F)
 
