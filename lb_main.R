eaffb4cc3a9c8412377b9ec065bbed17354add49
Luke Beebe <97060937+lukebeebe@users.noreply.github.com>
2024-02-15 20:43:16 -0500

    Add files via upload

--- lb_main.R
+++ lb_main.R
@@ -2,17 +2,17 @@
 library(lars)
 
 # NORMALIZE DATA PER SEASON
-# MAKE DELAY MAP CORRECT
 
 # main
-weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F){
+weather_prediction <- function(data, x, y, split=36, seas, year, lag=6, nn, sd=1){
+  year <- year+1
   df <- data[, append(x, y)]
-  rownames(df) <- seq(1, nrow(df), 1)
-  # if(norm==T){df <- scale(df[, x])} MOVE THIS TO RESPECTIVE SEASONS
+  y <- ncol(df)
+  rownames(df) <- seq(1, nrow(df), 1) %% 4
   df <- delay_map(df, lag)
-
-  # split data into past/future 9 seasons
-  split <- nrow(df)-36
+  
+  # split data into past/future 9 years
+  split <- nrow(df)-split
   df_train <- df[1:split,]
   df_test <- df[(split+1):nrow(df),]
   
@@ -22,14 +22,15 @@ weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F){
   # print(dim/2.5)
   
   # random samples to pull from columns
+  set.seed(12345)
   sample_cols <- delay_sample(ntrial=1200, (ncol(df_train)-1), dim)
   par(mfrow=c(length(seas),length(year)))
   ypreds <- resids <- paths <- NULL
   
   for(i in seas){
-    # seperate seasons
-    season_train <- df_train[seq(i, nrow(df_train), 4), ]
-    season_test <- df_test[seq(i, nrow(df_test), 4), ]
+    # separate seasons LOOK OVER SEASONS COMPARED TO DF
+    season_train <- df_train[rownames(df_train)==as.character(i%%4), ]
+    season_test <- df_test[rownames(df_test)==as.character(i%%4), ]
     
     for(j in year){
       # distance matrix
@@ -41,8 +42,10 @@ weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F){
       for(k in 1:length(sample_cols)){
         X <- nn_train[, sample_cols[[k]]]
         Y <- nn_train[, y]
+        ### 
         lars_output <- lars(X, Y)
         model_col <- match(TRUE, lars_output$Cp==min(lars_output$Cp))
+        
         if(is.na(model_col)){
           #TROUBLESHOOT NA VALUES
           next
@@ -60,11 +63,16 @@ weather_prediction <- function(data, x, y, seas, year, lag=6, nn, norm=F){
       ypreds <- na.omit(ypreds)
       preds <- sample(ypreds, 6000, replace=T) + sample(resids, 6000, replace=T)
       print(paste("NA values:", (5*1200-length(resids))/5))
+      
       den <- density(preds)
-      plot(den, frame=T, col='blue',main=paste(j-1,i), xlim=c(-3,3))
+      plot(den, frame=T, col='blue', main=paste(j-1,i))
       abline(v=y_test, col='red')
+      abline(v=y_test+stats$sd[i]*sd, col='grey', lty='dashed')
+      abline(v=y_test-stats$sd[i]*sd, col='grey', lty='dashed')
+      
       ypreds <- resids <- paths <- NULL
       
+      # OUTPUT LIST
     }
   }
 }
@@ -95,13 +103,18 @@ delay_sample <- function(ntrial=1200, ncol, nsel){
 }
 
 # finds euclidian distance between x rows and y row
-# IDEA - if too many dimensions, find distance of t-0, then t-1, then t-2...lower dimensions
+# IDEA - SEE DISTANCES FOR CERTAIN YEARS 
 nn_finder <- function(x, y, nn){
-  df <- rbind(y, x)
+  
+  df <- rbind(y, x) # SHOULD WE SCALE VALUES BEFORE FEEDING TO dist()
+  
   dist_order <- order(as.matrix(dist(df))[,1])
+  print(head(sort(as.matrix(dist(df))[,1])))
   nn_cols <- (match(2:6, dist_order)-1)
   nn_cols <- append(nn_cols, (match(7:31, dist_order)-1))
   return(nn_cols)
 }
 
-weather_prediction(hawaii_data, x=2:7, y=1, seas=c(1,2,3,4), year=c(1,2), lag=6, nn=30, norm=F)
+head(fresno_season)
+weather_prediction(as.matrix(fresno_season), x=c(3:9), y=10, seas=c(1,2,3,4), year=c(0,1), lag=6, nn=30, sd=0.5)
+ 
